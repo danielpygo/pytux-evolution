b582cdfa860b6ddb6461d7f05e77738e86aad458
daniel moore <ddmooris@cs.utexas.edu>
2018-11-16 17:23:12 -0600

    c

--- homework/train.py
+++ homework/train.py
@@ -2,7 +2,7 @@ import argparse, pickle
 import numpy as np
 import os
 from itertools import cycle
-
+import random
 import torch
 from torch import nn, optim
 
@@ -30,13 +30,16 @@ def train(epoch):
         '05 - The Somewhat Smaller Bath.stl' : 0.2,
     }
     print ("Num of parameters: ", sum([p.numel() for p in model.parameters() if p.requires_grad]))
-
     gaussians = {}
     survivors = []
     alpha_model = model
     # print(alpha_model.named_parameters())
-    for k,v in alpha_model.named_parameters():
-        gaussians[k] = (0,3)
+    trainable = {}
+    for name,p in alpha_model.named_parameters():
+        if p.requires_grad:
+            trainable[name] = p
+    # for k,v in trainable.items():
+    #     gaussians[k] = (0,3)
 
     for t in range(epoch):
 
@@ -47,19 +50,27 @@ def train(epoch):
         '''
         Your code here : optionally, you can print diagnostics of your model below
         '''
-
+        print("epoch", t)
         models_n = 20
         keep_models = 4
 
         models = [Model() for i in range(0,models_n)]
         for m in models:
-            params = {}
-            for k,v in alpha_model.named_parameters():
-                train_parameters[k] = v + numpy.random_normal(gaussians[k][0], gaussians[k][1])
-            m.load_state_dict(params)
+            params1 = {}
+            subdict = {}
+            for i in range(200):
+                k = random.choice(list(trainable))
+                subdict[k] = trainable[k]
+            for k,v in trainable.items():
+                params1[k] = v
+            for k,v in subdict.items():
+                params1[k] = v + np.random.normal(0,2)
+            m.load_state_dict(params1)
 
-        n_workers = 3
+        n_workers = 4
         H=500
+        iterations = 5
+        level = random.choice(list(levels))
         evaluators = [PolicyEvaluator.remote(level, iterations) for _ in range(n_workers)]
         rewards = ray.get([
         	evaluator.eval.remote(m, H) for m, evaluator in zip(models, evaluators)
@@ -67,22 +78,22 @@ def train(epoch):
 
         indices = np.argsort(rewards)[:keep_models]
         survivors = [models[i] for i in indices]
-        compute_new_gauss= {}
+        # compute_new_gauss = {}
         params = {}
         sum_mean = 0
         for survivor in survivors:
-            for k,v in survivor.named_parameters():
-                if k in compute_new:
-                    compute_new_gauss[k] = (0,2)
+            for k,v in trainable.items():
+                if k not in params:
+                    params[k] = v
                 else:
-                    compute_new_gauss[k][0] += v
-        for k,v in compute_new_gauss:
-            compute_new_gauss[k][0] /= 4
-            params[k] = compute_new_gauss[k][0]
+                    params[k] = torch.add(params[k],v)
+        for k,v in params.items():
+            # compute_new_gauss[k][0] /= 4
+            params[k] = torch.div(params[k],4)
         m = Model()
         m.load_state_dict(params)
         alpha_model = m
-        gaussians = compute_new_gausss
+        # gaussians = compute_new_gausss
 
         # Print diagnostics
         print ('====== Iter: %d ======' % t)
